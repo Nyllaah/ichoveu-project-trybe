37b9b79cc4202069ed95397702f91dbf453bb5c7
Nyllaah <nylla.g@hotmail.com>
2023-08-10 15:51:02 -0300

    feat: requirements 1 and 2, cypress not working

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,21 @@
-// Remova os comentários a medida que for implementando as funções
+const token = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const res = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`);
+  const data = await res.json();
+  if (data.length === 0) {
+    window.alert('Nenhuma cidade encontrada');
+  }
+  return data;
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
+export const getWeatherByCity = async (cityURL) => {
+  const res = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${token}&q=${cityURL}`);
+  const data = await res.json();
+  const { current: { temp_c: tempC, condition: { text, icon: ic } } } = data;
+  return console.log({
+    temp: tempC,
+    condition: text,
+    icon: ic,
+  });
 };
